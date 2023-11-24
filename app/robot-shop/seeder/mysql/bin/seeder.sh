fae694876ad4847129c135a9e8e596627ff22f6c
Hashfyre <joy.bhattacherjee@gmail.com>
2023-11-24 13:42:52 +0530

    fix-mysql-seeder

--- app/robot-shop/seeder/mysql/bin/seeder.sh
+++ app/robot-shop/seeder/mysql/bin/seeder.sh
@@ -7,7 +7,7 @@ MYSQL_PASSWORD="${MYSQL_PASSWORD}"
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SOURCE_DIR
 
-echo "Importing all seed data into DB: $DB_DEV"
+echo "Importing all seed data into DB: ${MYSQL_HOST}"
 for seed_file in *.sql; do
   echo "Adding file: $seed_file"
   MYSQL_PWD=$MYSQL_PASSWORD mysql -h "${MYSQL_HOST:-localhost}" -u $MYSQL_USER < $seed_file
