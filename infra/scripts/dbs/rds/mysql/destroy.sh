5a07d94350bcba85fddb88a21735fd7da92f062f
Hashfyre <joy.bhattacherjee@gmail.com>
2023-11-27 15:05:48 +0530

    refactor db-provisioning and destroy scripts

---
+++ infra/scripts/dbs/rds/mysql/destroy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+AWS_REGION=us-east-1
+
+aws rds delete-db-instance --db-instance-identifier robotshopmysql --skip-final-snapshot  --region ${AWS_REGION} --no-cli-pager
+aws rds wait db-instance-deleted --db-instance-identifier robotshopmysql  --region ${AWS_REGION} --no-cli-pager
+aws rds delete-db-subnet-group --db-subnet-group-name robotshopmysql-subnet-group --region ${AWS_REGION} --no-cli-pager
