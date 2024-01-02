f356fb4a12a257030e805225a8e797f61e187afc
saurabh3460 <saurabh3460@gmail.com>
2024-01-02 13:18:39 +0530

    add DB_SECURITY_GROUP_NAME varibale in common db script

--- infra/scripts/dbs/rds/common.sh
+++ infra/scripts/dbs/rds/common.sh
@@ -6,10 +6,10 @@ RDS_VPC_ID=$(aws eks describe-cluster --name ${CLUSTER_NAME} --region ${AWS_REGI
 
 echo "\nCreate security group..."
 # # ## Create security group
-aws ec2 create-security-group --group-name RobotShopRDSSecurityGroup --vpc-id ${RDS_VPC_ID} --description "RobotShop RDS security group" --region ${AWS_REGION} --no-cli-pager
+aws ec2 create-security-group --group-name ${DB_SECURITY_GROUP_NAME} --vpc-id ${RDS_VPC_ID} --description "RobotShop RDS security group" --region ${AWS_REGION} --no-cli-pager
 
 # # ## Get Security group ID; NOTE: This SG is shared b/w RDS and DocumentDB/MongoDB below
-RDS_VPC_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --region ${AWS_REGION}  --filters Name=group-name,Values=RobotShopRDSSecurityGroup --query 'SecurityGroups[*].GroupId' --output text)
+RDS_VPC_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --region ${AWS_REGION}  --filters Name=group-name,Values=${DB_SECURITY_GROUP_NAME} --query 'SecurityGroups[*].GroupId' --output text)
 
 # ## Add rule in security group
 CLUSTER_SG=$(aws eks describe-cluster --name ${CLUSTER_NAME} --region ${AWS_REGION} --query "cluster.resourcesVpcConfig.clusterSecurityGroupId" --output text)
