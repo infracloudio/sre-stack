579afe42fd97df7be22fd584716fb8d16f7ee8b9
saurabh3460 <saurabh3460@gmail.com>
2024-01-02 12:22:43 +0530

    add variables in rds create script and remove MYSQL_HOST

--- infra/scripts/dbs/rds/mysql/create.sh
+++ infra/scripts/dbs/rds/mysql/create.sh
@@ -15,31 +15,28 @@ aws ec2 authorize-security-group-ingress \
 ## Create subnet group
 echo "\nCreate DB subnet group..."
 # aws rds create-db-subnet-group --db-subnet-group-name robotshop-mysql-subnet-group --db-subnet-group-description "robotsho pmysql subnet group" --subnet-ids $SUBNET --region ${AWS_REGION}
-aws rds create-db-subnet-group --cli-input-json "{\"DBSubnetGroupName\":\"robotshop-mysql-subnet-group\",\"DBSubnetGroupDescription\":\"robotshop mysql subnet group\",\"SubnetIds\":$SUBNET}" --region ${AWS_REGION} --no-cli-pager
+aws rds create-db-subnet-group --cli-input-json "{\"DBSubnetGroupName\":\"${RDS_MYSQL_DB_SUBNET_GROUP_NAME}\",\"DBSubnetGroupDescription\":\"robotshop mysql subnet group\",\"SubnetIds\":$SUBNET}" --region ${AWS_REGION} --no-cli-pager
 
 ######## Create mysql db 
 echo "\nCreate MYSQL DB..."
 aws rds create-db-instance \
-  --db-name robotshopmysql \
-  --db-instance-identifier robotshopmysql \
-  --allocated-storage 10 \
-  --db-instance-class db.t2.micro \
+  --db-name ${RDS_MYSQL_DB_NAME} \
+  --db-instance-identifier ${RDS_MYSQL_DB_NAME} \
+  --allocated-storage ${RDS_MYSQL_DB_STORAGE} \
+  --db-instance-class ${RDS_MYSQL_DB_INSTANCE_CLASS} \
   --engine mysql \
-  --engine-version "5.7.37" \
-  --master-username admin \
-  --master-user-password docdb3421z \
+  --engine-version ${RDS_MYSQL_DB_ENGINE_VERSION} \
+  --master-username ${RDS_MYSQL_DB_MASTER_USERNAME} \
+  --master-user-password ${RDS_MYSQL_DB_MASTER_PASSWORD} \
   --no-publicly-accessible \
   --vpc-security-group-ids ${RDS_VPC_SECURITY_GROUP_ID} \
-  --db-subnet-group-name "robotshop-mysql-subnet-group" \
-  --db-parameter-group-name sre-stack-mysql57 \
+  --db-subnet-group-name ${RDS_MYSQL_DB_SUBNET_GROUP_NAME} \
+  --db-parameter-group-name ${RDS_MYSQL_DB_PARAMETER_GROUP_NAME} \
   --backup-retention-period 0 \
   --region ${AWS_REGION} \
-  --tags Key=name,Value=robotshopmysql \
+  --tags Key=name,Value=${RDS_MYSQL_DB_NAME} \
   --port 3306 \
   --no-cli-pager
 
 echo "\n Wait for MYSQL DB..."
-aws rds wait db-instance-available --db-instance-identifier robotshopmysql  --region ${AWS_REGION} 
-
-## GET DB endpoint
-export MYSQL_HOST=$(aws rds describe-db-instances --db-instance-identifier robotshopmysql --region ${AWS_REGION} --query 'DBInstances[*].Endpoint.Address' --output text)
+aws rds wait db-instance-available --db-instance-identifier ${RDS_MYSQL_DB_NAME}  --region ${AWS_REGION} 
