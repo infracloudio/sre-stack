10743e305e71fe67f980a0af745fa9783c189953
saurabh3460 <saurabh3460@gmail.com>
2024-01-02 13:52:43 +0530

    add source .env and update/add variables in scenario-02

--- scenarios/scenario-02/scripts/modify-rds-param.sh
+++ scenarios/scenario-02/scripts/modify-rds-param.sh
@@ -1,20 +1,21 @@
-SCENARIO_TIMEOUT=2m
-AWS_REGION=us-east-1
+#!/bin/bash
+GIT_TLD=`git rev-parse --show-toplevel`
+source ${GIT_TLD}/.env
 
 echo "Reducing Database max_connection..."
 
 aws rds modify-db-parameter-group \
-    --db-parameter-group-name sre-stack-mysql57 \
+    --db-parameter-group-name ${RDS_MYSQL_DB_PARAMETER_GROUP_NAME} \
     --parameters "ParameterName=max_connections,ParameterValue=10,ApplyMethod=immediate" \
     --region ${AWS_REGION} \
     --no-cli-pager
 
-sleep $SCENARIO_TIMEOUT
+sleep ${SCENARIO_02_TIMEOUT}
 
 echo "\n Undo Database max_connection..."
 
 aws rds modify-db-parameter-group \
-    --db-parameter-group-name sre-stack-mysql57 \
+    --db-parameter-group-name ${RDS_MYSQL_DB_PARAMETER_GROUP_NAME} \
     --parameters "ParameterName=max_connections,ParameterValue='{DBInstanceClassMemory/12582880}',ApplyMethod=immediate" \
     --region ${AWS_REGION} \
     --no-cli-pager
